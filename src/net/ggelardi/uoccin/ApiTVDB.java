7e83ce52d9293ac97773797b6da3c24146ee8947
tarzasai <ggelardi@gmail.com>
2015-02-02 18:56:40 +0100

    .

--- src/net/ggelardi/uoccin/ApiTVDB.java
+++ src/net/ggelardi/uoccin/ApiTVDB.java
@@ -29,7 +29,7 @@ import android.text.TextUtils;
 public class ApiTVDB {
 	private static final String API_ENDPOINT = "http://thetvdb.com/api/A74D017DA5F2C3B0";
 	
-	interface TVDB {
+	public interface TVDB {
 		
 		@GET("/series/{tvdb_id}/all/{language}.xml")
 		void getSeriesFull(@Path("tvdb_id") int tvdb_id, @Path("language") String language, Callback<Series> callback);
@@ -43,15 +43,15 @@ public class ApiTVDB {
 		
 	}
 	
-	static class BaseType {
-		@Element(name = "id")
-		int tvdb_id;
-		
+	public static class BaseType {
 		@Element(name = "IMDB_ID")
 		String imdb_id;
 		
 		@Element(name = "Language")
 		String language;
+		
+		@Element(name = "Rating")
+		public double rating;
 		
 		@Element(name = "lastupdated")
 		long lastupdated = System.currentTimeMillis();
@@ -62,63 +62,120 @@ public class ApiTVDB {
 	}
 	
 	@Root(name = "Series")
-	static class Series extends BaseType {
+	public static class Series extends BaseType {
+		@Element(name = "id")
+		public int tvdb_id;
+		
 		@Element(name = "SeriesName")
-		String name;
+		public String name;
 
 		@Element(name = "Overview")
-		String overview;
+		public String overview;
 
 		@Element(name = "Network")
-		String network;
+		public String network;
 
 		@Element(name = "Status")
-		String status;
+		public String status;
 
 		@Element(name = "FirstAired")
-		Date firstAired;
+		public Date firstAired;
 
 		@Element(name = "ContentRating")
-		String contentRating;
-
-		@Element(name = "Rating")
-		double rating;
-
+		public String contentRating;
+		
 		@Element(name = "Airs_DayOfWeek")
 		@Convert(DayName2IntConverter.class)
-		int airsDay;
+		public int airsDay;
 
 		@Element(name = "Airs_Time")
-		long airsTime;
+		public long airsTime;
 
 		@Element(name = "Runtime")
-		int runtime;
+		public int runtime;
 
 		@Element(name = "poster")
-		String poster;
+		public String poster;
 
 		@Element(name = "banner")
-		String banner;
+		public String banner;
 
 		@Element(name = "fanart")
-		String fanart;
+		public String fanart;
 
 		@Element(name = "zap2it_id")
-		String zap2it_id;
+		public String zap2it_id;
 		
 		@Element(name = "Genre")
 		@Convert(PipedStringsConverter.class)
-		List<String> Genres;
+		public List<String> Genres;
 
 		@Element(name = "Actors")
 		@Convert(PipedStringsConverter.class)
-		List<String> Actors;
+		public List<String> Actors;
 		
 		@ElementList(entry="Episode", inline=true)
-		List<Episode> episodes;
+		public List<Episode> episodes;
 	}
 	
-	static class Episode extends BaseType {
+	/*
+	
+  <Episode>
+    <id>5050213</id>
+    <seriesid>248742</seriesid>
+    <seasonid>589267</seasonid>
+    <SeasonNumber>4</SeasonNumber>
+    <EpisodeNumber>13</EpisodeNumber>
+    <EpisodeName>M.I.A.</EpisodeName>
+    <Overview>Reese and Root’s hunt for Shaw takes them to a small town in upstate New York where it becomes apparent that not everything is as idyllic as it seems. Also, Fusco teams with a former POI to tackle the newest number.</Overview>
+    <FirstAired>2015-02-03</FirstAired>
+    <Writer>Lucas O'Connor</Writer>
+    <Director>Kevin Bray</Director>
+    <GuestStars></GuestStars>
+    <filename>episodes/248742/5050213.jpg</filename>
+    <thumb_width>400</thumb_width>
+    <thumb_height>225</thumb_height>
+    <EpImgFlag>2</EpImgFlag>
+  </Episode>
+	
+	*/
+
+	@Root(name = "Episode")
+	public static class Episode extends BaseType {
+		@Element(name = "seriesid")
+		public int tvdb_id;
+		
+		@Element(name = "seasonid")
+		public int season_id;
+		
+		@Element(name = "id")
+		public int episode_id;
+		
+		@Element(name = "SeasonNumber")
+		public int seasonNumber;
+		
+		@Element(name = "EpisodeNumber")
+		public int episodeNumber;
+		
+		public String getEpisodeID() {
+			return String.format("S%1$02dE%2$02d", seasonNumber, episodeNumber);
+		}
+		
+		@Element(name = "Overview")
+		public String overview;
+		
+		@Element(name = "FirstAired")
+		public Date firstAired;
+
+		@Element(name = "Writer")
+		public String writer;
+		
+		@Element(name = "Director")
+		public String director;
+		
+		@Element(name = "GuestStars")
+		@Convert(PipedStringsConverter.class)
+		public List<String> guestStars;
 	}
 	
 	private static TVDB TVDB_CLIENT;
@@ -239,16 +296,16 @@ http://thetvdb.com/api/A74D017DA5F2C3B0/series/248742/default/4/13/en.xml
 <Data>
   <Episode>
     <id>5050213</id>
-    <seasonid>589267</seasonid>
     <seriesid>248742</seriesid>
+    <seasonid>589267</seasonid>
     <SeasonNumber>4</SeasonNumber>
     <EpisodeNumber>13</EpisodeNumber>
     <EpisodeName>M.I.A.</EpisodeName>
-    <FirstAired>2015-02-03</FirstAired>
-    <GuestStars></GuestStars>
-    <Director>Kevin Bray</Director>
-    <Writer>Lucas O'Connor</Writer>
     <Overview>Reese and Root’s hunt for Shaw takes them to a small town in upstate New York where it becomes apparent that not everything is as idyllic as it seems. Also, Fusco teams with a former POI to tackle the newest number.</Overview>
+    <FirstAired>2015-02-03</FirstAired>
+    <Writer>Lucas O'Connor</Writer>
+    <Director>Kevin Bray</Director>
+    <GuestStars></GuestStars>
     <lastupdated>1421964803</lastupdated>
     <filename>episodes/248742/5050213.jpg</filename>
     <thumb_width>400</thumb_width>
