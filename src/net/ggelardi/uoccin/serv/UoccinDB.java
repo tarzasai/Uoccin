418478b9781f0aa7ee14d5b28abb6cded60d0bf1
tarzasai <ggelardi@gmail.com>
2015-02-06 17:57:15 +0100

    .

--- src/net/ggelardi/uoccin/serv/UoccinDB.java
+++ src/net/ggelardi/uoccin/serv/UoccinDB.java
@@ -13,35 +13,69 @@ public class UoccinDB extends SQLiteOpenHelper {
 		super(context, NAME, null, VERSION);
 	}
 	
-	/*
-	public UoccinDB(Context context, String name, CursorFactory factory, int version) {
-		super(context, name, factory, version);
-		// TODO Auto-generated constructor stub
-	}
-	
-	public UoccinDB(Context context, String name, CursorFactory factory, int version, DatabaseErrorHandler errorHandler) {
-		super(context, name, factory, version, errorHandler);
-		// TODO Auto-generated constructor stub
-	}
-	*/
-	
 	@Override
 	public void onCreate(SQLiteDatabase db) {
-		// TODO Auto-generated method stub
-		
+		db.execSQL(CREATE_TABLE_TITLE);
+		db.execSQL(CREATE_TABLE_SERIES);
+		db.execSQL(CREATE_TABLE_EPISODES);
 	}
 	
 	@Override
 	public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
-		// TODO Auto-generated method stub
-		
+		if (newVersion < 1) {
+			// Development phase: destroy everything.
+			db.execSQL("drop table episodes");
+			db.execSQL("drop table series");
+			db.execSQL("drop table title");
+			onCreate(db);
+		}
 	}
 	
 	private static final String CS = ", ";
 	private static final String PK = " PRIMARY KEY NOT NULL";
 	private static final String DT_INT = " INTEGER";
 	private static final String DT_STR = " TEXT";
+	private static final String DT_FLG = " INTEGER NOT NULL DEFAULT 0";
 	private static final String DT_DAT = " TEXT";
 	private static final String CC_NNU = " NOT NULL";
 	
+	private static final String CREATE_TABLE_TITLE = "CREATE TABLE title (" +
+		"id" + DT_INT + PK + " AUTOINCREMENT" + CS +
+		"type" + DT_STR + CC_NNU + " CHECK (type IN ('movie', 'show', 'episode'))" + CS +
+		"name" + DT_STR + CC_NNU + CS +
+		"year" + DT_INT + CC_NNU + CS +
+		"plot" + DT_STR + CS +
+		"imdb_id" + DT_STR + CC_NNU + CS +
+		"language" + DT_STR + CC_NNU + " DEFAULT 'en'" + CS +
+		"director" + DT_STR + CS +
+		"writers" + DT_STR + CS +
+		"actors" + DT_STR + CS +
+		"poster" + DT_STR + CS +
+		"banner" + DT_STR + CS +
+		"rating" + DT_INT + CS +
+		"runtime" + DT_INT + CS +
+		"updated" + DT_DAT + CC_NNU + " DEFAULT CURRENT_TIMESTAMP" + CS +
+		"watchlist" + DT_FLG + CS +
+		"collected" + DT_FLG + CS +
+		"watched" + DT_FLG + CS +
+		")";
+	
+	private static final String CREATE_TABLE_SERIES = "CREATE TABLE series (" +
+		"id" + DT_INT + PK + CS +
+		"tvdb_id" + DT_INT + CC_NNU + CS +
+		"status" + DT_STR + CC_NNU + " CHECK (status IN ('continuing', 'ended'))" + CS +
+		"network" + DT_STR + CS +
+		"airsDay" + DT_INT + CS +
+		"airsTime" + DT_STR + CS +
+		"firstAired" + DT_DAT + CS +
+		"contentRating" + DT_STR + CS +
+		")";
+	
+	private static final String CREATE_TABLE_EPISODES = "CREATE TABLE episodes (" +
+		"id" + DT_INT + PK + CS +
+		"tvdb_id" + DT_INT + CC_NNU + CS +
+		"season" + DT_INT + CC_NNU + CS +
+		"number" + DT_INT + CC_NNU + CS +
+		"firstAired" + DT_DAT + CS +
+		")";
 }
