e939fb455094aa9241fa3378878ed730dbfa4630
tarzasai <ggelardi@gmail.com>
2015-02-25 00:30:34 +0100

    .

--- src/net/ggelardi/uoccin/data/SeriesAdapter.java
+++ src/net/ggelardi/uoccin/data/SeriesAdapter.java
@@ -2,12 +2,10 @@ package net.ggelardi.uoccin.data;
 
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Locale;
 
 import net.ggelardi.uoccin.R;
 import net.ggelardi.uoccin.serv.Session;
 import android.content.Context;
-import android.text.TextUtils;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -55,8 +53,10 @@ public class SeriesAdapter extends BaseAdapter {
 			vh.txt_ser_name = (TextView) view.findViewById(R.id.txt_ser_name);
 			vh.txt_ser_info = (TextView) view.findViewById(R.id.txt_ser_info);
 			vh.txt_ser_plot = (TextView) view.findViewById(R.id.txt_ser_plot);
+			/*
 			vh.txt_ser_actors = (TextView) view.findViewById(R.id.txt_ser_actors);
 			vh.txt_ser_genres = (TextView) view.findViewById(R.id.txt_ser_genres);
+			*/
 			view.setTag(vh);
 		} else {
 			vh = (ViewHolder) view.getTag();
@@ -64,8 +64,9 @@ public class SeriesAdapter extends BaseAdapter {
 		Series ser = getItem(position);
 		session.picasso().load(ser.poster).placeholder(R.drawable.ic_action_image).fit().into(vh.img_ser_poster);
 		vh.txt_ser_name.setText(ser.name);
-		vh.txt_ser_info.setText(ser.infoLine());
+		vh.txt_ser_info.setText(ser.network);
 		vh.txt_ser_plot.setText(ser.plot);
+		/*
 		if (ser.actors.isEmpty())
 			vh.txt_ser_actors.setVisibility(View.GONE);
 		else {
@@ -78,6 +79,7 @@ public class SeriesAdapter extends BaseAdapter {
 			vh.txt_ser_genres.setVisibility(View.VISIBLE);
 			vh.txt_ser_genres.setText(TextUtils.join(", ", ser.genres).toLowerCase(Locale.getDefault()));
 		}
+		*/
 		return view;
 	}
 	
