b83bd37445fa0259d1881b864c8d920cde375d61
tarzasai <ggelardi@gmail.com>
2015-05-05 02:27:52 +0200

    .

--- src/net/ggelardi/uoccin/api/XML.java
+++ src/net/ggelardi/uoccin/api/XML.java
@@ -74,7 +74,7 @@ public class XML {
 			@GET("/?type=movie&r=xml")
 			Document findMovie(@Query("s") String text);
 			
-			@GET("/?plot=full&r=xml")
+			@GET("/?plot=full&r=xml&tomatoes=true")
 			Document getMovie(@Query("i") String imdb_id);
 		}
 		
